87e2e103af57f79a9b365629ac20e074142d58ab
Nicolás Plazas <67563538+nicolas-plazas@users.noreply.github.com>
2021-07-30 08:27:37 -0500

    Add files via upload

--- tarea_1/punto_5/ordenar_arreglo.py
+++ tarea_1/punto_5/ordenar_arreglo.py
@@ -1,11 +1,7 @@
 def run():
     # Listas desorganizadas
-    lista_texto = ['mouse', 'teclado', 'computador', '*']
-    print(f'Lista con string en desorden {lista_texto}')
-    lista_texto.sort()
-    print(f'Lista con string en organizada {lista_texto}')
-    lista_enteros= [100, 1, 50]
-    print(f'Lista con enteros en desorden {lista_enteros} y organizada {lista_enteros.sort()}')
+    lista = ['mouse', 'teclado', 'computador', '*', 100, 1, 50]
+    print(sorted(lista, key=str))
 
 if __name__ == '__main__':
     run()
